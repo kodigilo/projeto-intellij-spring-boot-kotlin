40577f3f3c360afd642690748ea63e62461b2b17
Gabriel Alencar <gabriel@alencar.site>
2020-10-12 02:42:37 -0300

    Start

---
+++ src/main/kotlin/linguagem/digital/demo/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package linguagem.digital.demo
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(SpringKotlinApplication::class.java)
+    }
+
+}
